0a845f4705d073d6cdd1c02b417a95ee5f2aaf3d
Hackbright Student <info@hackbrightacademy.com>
2016-04-19 12:57:23 -0700

    Give user selected and random compliment

--- nice.py
+++ nice.py
@@ -16,8 +16,17 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "Hi! This is the home page."
-
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Homepage</title>
+      </head>
+      <body>
+        <p>Hi! This is the home page. Let's <a href="/hello">get started</a>!</p>
+      </body>
+    </html>
+    """
 
 @app.route('/hello')
 def say_hello():
@@ -32,7 +41,11 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          <label>What's your name? <input type="text" name="person"></label>
+          <label>What's your name? <input type="text" name="person"></label><br>
+          You are best described as: <input type="checkbox" name="user-compliment" value="smart">smart
+          <input type="checkbox" name="user-compliment" value="dope">dope
+          <input type="checkbox" name="user-compliment" value="witty">witty
+          <input type="checkbox" name="user-compliment" value="really, really, really good-looking">really, really, really good-looking<br>
           <input type="submit">
         </form>
       </body>
@@ -45,6 +58,7 @@ def greet_person():
     """Get user by name."""
 
     player = request.args.get("person")
+    user_compliment = request.args.get("user-compliment")
 
     compliment = choice(AWESOMENESS)
 
@@ -55,10 +69,10 @@ def greet_person():
         <title>A Compliment</title>
       </head>
       <body>
-        Hi %s I think you're %s!
+        Hi %s you're %s and I think you're %s!
       </body>
     </html>
-    """ % (player, compliment)
+    """ % (player, user_compliment, compliment)
 
 
 if __name__ == '__main__':
